708e61d77d307c1fe6d8f9f95dc2f9179ba10146
Diogo Correia <diogo.correia99@ua.pt>
2022-10-15 15:07:44 +0100

    began transition from php to django

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # /missions/
+    #path('', views.missions),
+	# /missions/missionId/
+    #path('<slug:value>', views.missions),
+]
