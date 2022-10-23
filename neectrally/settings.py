a1ae643dd2be97ca9a9e2b0ac5dc6a6d50d800de
Diogo Correia <diogo.correia99@ua.pt>
2022-10-23 21:03:01 +0100

    minor fixes

--- neectrally/settings.py
+++ neectrally/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
