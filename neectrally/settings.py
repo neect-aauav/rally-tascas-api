e941fed139c24e7e759ba54fbf3e537080a75ca7
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-10-28 22:39:29 +0100

    changed static_root

--- neectrally/settings.py
+++ neectrally/settings.py
@@ -18,7 +18,7 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR =  Path(os.environ.get('PROD_BASE_DIR')).as_posix() if os.environ.get('PROD_BASE_DIR') else Path(_file_).resolve().parent.parent
+BASE_DIR =  Path(os.environ.get('PROD_BASE_DIR')).as_posix() if os.environ.get('PROD_BASE_DIR') else Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -155,8 +155,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR  / 'staticfiles'
-#STATIC_ROOT = os.environ.get('PROD_BASE_DIR') if BASE_DIR + '/staticfiles' else BASE_DIR  / 'staticfiles'
+#STATIC_ROOT = BASE_DIR  / 'staticfiles'
+STATIC_ROOT = BASE_DIR + '/staticfiles' if os.environ.get('PROD_BASE_DIR') else BASE_DIR  / 'staticfiles'
 
 STATIC_URL = 'static/'
 
