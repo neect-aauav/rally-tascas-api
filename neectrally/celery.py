6004e63753376d11f183d183c003e2a99c4100a5
Diogo Correia <diogo.correia99@ua.pt>
2022-10-31 06:33:46 +0000

    trying to reduce redis connections

--- neectrally/celery.py
+++ neectrally/celery.py
@@ -5,13 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neectrally.settings')
 
 app = celery.Celery('rallytascas')
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
-
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
