30667c54352440ec92e7ef4f706667863afe00f5
Diogo Correia <diogo.correia99@ua.pt>
2022-10-31 06:10:30 +0000

    fixed issue with celery configs

--- neectrally/celery.py
+++ neectrally/celery.py
@@ -5,7 +5,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neectrally.settings')
 
 app = celery.Celery('rallytascas')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
 
